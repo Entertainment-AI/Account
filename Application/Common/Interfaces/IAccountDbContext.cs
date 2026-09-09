5c10551ca186cc6516647cce68b3183e7a8969fa
Khánh Hưng <khanhhungzm@gmail.com>
2026-09-09 09:46:06 +0700

    feat(application): register subscription, order, and wallet DbSets in IAccountDbContext

--- Application/Common/Interfaces/IAccountDbContext.cs
+++ Application/Common/Interfaces/IAccountDbContext.cs
@@ -6,5 +6,10 @@ namespace Account.Application.Common.Interfaces;
 public interface IAccountDbContext
 {
     DbSet<User> Users { get; }
+    DbSet<Plan> Plans { get; }
+    DbSet<UserSubscription> UserSubscriptions { get; }
+    DbSet<Order> Orders { get; }
+    DbSet<Wallet> Wallets { get; }
+    DbSet<WalletTransaction> WalletTransactions { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
